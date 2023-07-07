75b64747990cbe767d20a2acb699e5fc58fccc21
Saniel1990 <saniel_sabr@outlook.com>
2023-07-07 09:14:25 -0300

    feat: recommended changes

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,39 +1,49 @@
-import 'dotenv/config'
-import 'reflect-metadata'
-import { DataSource, DataSourceOptions } from 'typeorm'
-import path from 'path'
+import "dotenv/config";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+import path from "path";
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath: string = path.join(__dirname, './entities/**.{ts,js}')
-  const migrationsPath: string = path.join(__dirname, './migrations/**.{ts,js}')
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{ts,js}"
+  );
 
-  const dbUrl: string | undefined = process.env.DATABASE_URL
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
+  const dbUrl: string | undefined = process.env.DATABASE_URL;
 
-  if (!dbUrl) {
-    throw new Error('Env var DATABASE_URL does not exists')
-  }
+  if (!nodeEnv) throw new Error("Env var NODE_ENV does not exists");
+  if (!dbUrl) throw new Error("Env var DATABASE_URL does not exists");
 
-  const nodeEnv: string | undefined = process.env.NODE_ENV
-
-  if (nodeEnv === 'test') {
+  if (nodeEnv === "test") {
     return {
-      type: 'sqlite',
-      database: ':memory:',
+      type: "sqlite",
+      database: ":memory:",
       synchronize: true,
       entities: [entitiesPath],
-    }
+    };
+  }
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: dbUrl,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
   }
 
   return {
-    type: 'postgres',
+    type: "postgres",
     url: dbUrl,
     synchronize: false,
     logging: true,
     migrations: [migrationsPath],
     entities: [entitiesPath],
-  }
-}
+  };
+};
 
-const AppDataSource = new DataSource(dataSourceConfig())
+const AppDataSource = new DataSource(dataSourceConfig());
 
-export { AppDataSource }
+export { AppDataSource };
